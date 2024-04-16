7e74f071cdb9e6a7c0100c0bd73b642bc2158d52
Desislava Petrova <des612@abv.bg>
2024-04-16 11:48:00 +0300

    Initial files

---
+++ blabbook/src/views/Home/Home.tsx
@@ -0,0 +1,12 @@
+import CreateAccount from '../../components/CreateAccount/CreateAccount'
+
+const Home = () => {
+
+    return (
+        <div className="text-lg">
+        Hello
+        </div>
+    );
+} 
+
+export default Home;
---
+++ blabbook/tailwind.config.ts
@@ -0,0 +1,10 @@
+import daisyui from 'daisyui';
+
+export default {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [daisyui],
+};
